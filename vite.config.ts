a709c198dd179083a1f049f6ba88f31720e09c7b
Titiksha Pal <titikshapal765@gmail.com>
2026-01-07 17:48:37 +0530

    moving to authentication and introduction to puter  js

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,18 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix for "__dirname is not defined" in ES modules
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // This line forces any import starting with "~/constants" to look in your root folder
+      "~/constants": path.resolve(__dirname, "./constants"),
+    },
+  },
 });
